651792b7074e8395f767f8d92da960b57047b005
Felipe Wesley <fe.wesleybasso@gmail.com>
2020-10-27 22:50:29 -0300

    Create initial structure of the index page

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -8,54 +8,59 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+	<!-- Icon to navigator tab -->
+	<link rel="shortcut icon" href="./content/images/icon/favicon.png" type="image/x-icon">
+
 	<!-- Bootstrap CSS -->
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+	<!-- Customized CSS to this page -->
+	<link rel="stylesheet" href="./content/css/index/index-style.css">
+
+	<!-- Script to Font Awesome 5 PRO -->
+	<script src="https://kit.fontawesome.com/a076d05399.js"></script>
 
 	<title>:: University Market</title>
 </head>
 <body class="bg-light">
 
 	<!-- Image and text -->
-	<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+	<nav class="navbar navbar-dark bg-primary">
 		<a class="navbar-brand" href="#">
-			<img src="./content/images/bootstrap-icon.png" width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy">
-			Bootstrap
+			<img src="./content/images/logo-project.png" width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy">
+			<strong>University Market</strong>
 		</a>
-		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-			<span class="navbar-toggler-icon"></span>
-		</button>
-	<!-- <nav class="navbar navbar-light bg-primary"> -->
-		<div class="collapse navbar-collapse" id="navbarSupportedContent">
-			<ul class="navbar-nav mr-auto">
-				<li class="nav-item active">
-					<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link" href="#">Link</a>
-				</li>
-				<li class="nav-item dropdown">
-					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-						Dropdown
+		<div class="float-right">
+			<div class="dropdown">
+				<a class="nav-link text-light dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+					<i class="fas fa-user"></i>
+				</a>
+				<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
+					<a class="dropdown-item" href="#">
+						<i class="far fa-user"></i>
+						Perfil
 					</a>
-					<div class="dropdown-menu" aria-labelledby="navbarDropdown">
-						<a class="dropdown-item" href="#">Action</a>
-						<a class="dropdown-item" href="#">Another action</a>
-						<div class="dropdown-divider"></div>
-						<a class="dropdown-item" href="#">Something else here</a>
-					</div>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-				</li>
-			</ul>
-			<form class="form-inline my-2 my-lg-0">
-				<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-				<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-			</form>
+					<div class="dropdown-divider"></div>
+					<a class="dropdown-item bg-danger text-light" href="#">
+						<i class="fas fa-power-off"></i>
+						Sair
+					</a>
+				</div>
+			</div>
 		</div>
 	</nav>
 	
-	<h1>Hello, world!</h1>
+	<div class="welcome-div">
+		<h1 class="display-4">Hello, world!</h1>
+		<p class="lead">
+			This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.
+		</p>
+		<hr class="my-4">
+		<p>
+			It uses utility classes for typography and spacing to space content out within the larger container.
+		</p>
+		<a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+	</div>
 
 
 	<!-- Optional JavaScript; choose one of the two! -->
@@ -66,5 +71,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
 	-->
+	<!-- Customized JavaScript files -->
+	<script src="./content/js/index/index-script.js"></script>
 </body>
 </html>
