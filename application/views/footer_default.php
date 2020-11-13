cdf5631f2849f8a4b4a6d44cdbe506b6736494f7
Felipe Wesley <fe.wesleybasso@gmail.com>
2020-11-13 01:40:16 -0300

    create default header and footer for all pages

---
+++ application/views/footer_default.php
@@ -0,0 +1,26 @@
+<!-- Responsive menu model navbar (only for JS) -->
+<!-- <nav id="navbar-bottom-mobile"></nav> -->
+
+<nav class="navbar fixed-bottom navbar-light bg-light" id="navbar-bottom-mobile">
+
+	<?php foreach($menu_data as $menu_item) : ?>
+		<a class="nav-link" href="<?=$menu_item['href'];?>" 
+			<?php echo isset($menu_item['help']) ? "title=\"{$menu_item['help']}\"":"";?> >
+			<i class="<?=$menu_item['icon'];?>"></i>
+		</a>
+
+	<?php endforeach; ?>
+
+</nav>
+
+<!-- Optional JavaScript; choose one of the two! -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+<!-- Option 2: jQuery, Popper.js, and Bootstrap JS
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+-->
+
+</body>
+</html>
---
+++ application/views/header_default.php
@@ -0,0 +1,54 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+<!doctype html>
+<html lang="pt-BR">
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- Icon to navigator tab -->
+	<!-- <link rel="shortcut icon" href="/content/images/icon/favicon.png" type="image/x-icon"> -->
+	<link rel="shortcut icon" href="/content/images/icon/favicon.png" type="image/x-icon">
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+	<!-- Default CSS of University Market page -->
+	<link rel="stylesheet" href="/content/css/master/default-style.css">
+
+	<!-- Script to Font Awesome 5 PRO -->
+	<script src="https://kit.fontawesome.com/a076d05399.js"></script>
+
+	<title>:: University Market</title>
+</head>
+<body class="bg-light">
+
+	<!-- Image and text -->
+	<nav class="navbar navbar-dark bg-primary">
+		<a class="navbar-brand" href="#">
+			<!-- <img src="/content/images/logo-project.png" width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy"> -->
+			<img src="/content/images/logo-project.png" width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy">
+			<strong>University Market</strong>
+		</a>
+		<div class="float-right">
+			<a class="nav-link text-light" href="#" title="Logout" id="logout_btn">
+				Logout <i class="fas fa-sign-out-alt"></i>
+			</a>
+		</div>
+	</nav>
+	
+	<!-- Desktop menu model navbar (only for JS) -->
+	<!-- <nav id="navbar-top-desktop"></nav> -->
+
+	<nav class="navbar sticky-top justify-content-center navbar-light navbar-block bg-light" id="navbar-top-desktop">
+
+		<?php foreach($menu_data as $menu_item) : ?>
+			<a class="nav-link" href="<?=$menu_item['href'];?>" 
+				<?php echo isset($menu_item['help']) ? "title=\"{$menu_item['help']}\"":"";?> >
+				<i class="<?=$menu_item['icon'];?>"></i>
+				<span><?=$menu_item['title'];?></span>
+			</a>
+
+		<?php endforeach; ?>
+
+	</nav>
