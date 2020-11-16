9b46bbf2355e31ecb8f8bc1cfb77852a32c888dc
Felipe Wesley <fe.wesleybasso@gmail.com>
2020-11-16 15:18:47 -0300

    change default style on homepage

--- application/views/homepage.php
+++ application/views/homepage.php
@@ -50,11 +50,11 @@
 
 				<?php endfor; ?>
 			</div>
-			<a class="carousel-control-prev" href="#carousel-example" role="button" data-slide="prev">
+			<a class="carousel-control-prev carousel-control-ico" href="#carousel-example" role="button" data-slide="prev">
 				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
 				<span class="sr-only">Previous</span>
 			</a>
-			<a class="carousel-control-next" href="#carousel-example" role="button" data-slide="next">
+			<a class="carousel-control-next carousel-control-ico" href="#carousel-example" role="button" data-slide="next">
 				<span class="carousel-control-next-icon" aria-hidden="true"></span>
 				<span class="sr-only">Next</span>
 			</a>
